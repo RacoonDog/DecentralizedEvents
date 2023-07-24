eae0ffe135ad83753cc70c5d23bbbc3c9c9118a6
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2023-07-23 22:53:32 -0400

    oops

--- src/main/java/io/github/racoondog/decentralizedevents/exceptions/EventTypeNotRegisteredException.java
+++ src/main/java/io/github/racoondog/decentralizedevents/exceptions/EventTypeNotRegisteredException.java
@@ -3,7 +3,7 @@ package io.github.racoondog.decentralizedevents.exceptions;
 import io.github.racoondog.decentralizedevents.events.Event;
 
 public class EventTypeNotRegisteredException extends RuntimeException {
-    public EventTypeNotRegisteredException(Class<? extends Event<?, ?>> eventClass) {
+    public EventTypeNotRegisteredException(Class<? extends Event<?>> eventClass) {
         super("Event type '" + eventClass.getName() + "' was not registered to the event bus.");
     }
 }
